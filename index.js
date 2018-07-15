e22bcb9a73f912e24db95f2491fc34a843825e17
Tyler Boright <lupin3.ken@gmail.com>
2018-07-15 13:25:02 -0700

    adds an exported map of all of the apple products pixel sizes

---
+++ index.js
@@ -0,0 +1,22 @@
+module.exports = {
+	smallPhone: {
+		height: '568px',
+		width: '320px'
+	},
+	mediumPhone: {
+		height: '667px',
+		width: '375px'
+	},
+	largePhone: {
+		height: '736px',
+		width: '414px'
+	},
+	smallTablet: {
+		height: '1024px',
+		width: '768px'
+	},
+	largeTablet: {
+		height: '1366px',
+		width: '1024px'
+	}
+}
